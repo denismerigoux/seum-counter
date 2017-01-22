0add8e5942729ed0157d093a956464e531a38410
Camille Masset <masset.camille@gmail.com>
2017-01-22 02:21:19 +0100

    Add settings for i18n in settings.default

--- counter/views/home.py
+++ counter/views/home.py
@@ -1,25 +1,31 @@
-from django.shortcuts import render
-from django.utils.translation import ugettext as _
-from counter.models import Counter, Reset, Like, Keyword, Hashtag
-from django.contrib.auth.models import User
-from babel.dates import format_timedelta, format_datetime
+import copy
 from datetime import datetime, timedelta
+import functools
+import math
+import random
+
 from django import forms
-from django.http import HttpResponseRedirect
-from django.core.mail import EmailMessage
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
 from django.core.urlresolvers import reverse
 from django.db import IntegrityError
-from graphos.renderers import gchart
+from django.db.models import Prefetch, Count
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.template.loader import render_to_string
-from graphos.sources.simple import SimpleDataSource
-from graphos.sources.model import ModelDataSource
-import random
-import math
-import copy
-import functools
 from django.utils import timezone
+from django.utils.translation import ugettext as _, get_language
+
+import arrow
+from babel.dates import format_timedelta, format_datetime
+from graphos.renderers import gchart
+from graphos.sources.model import ModelDataSource
+from graphos.sources.simple import SimpleDataSource
+
+from counter.models import *
 from counter.utils import parseSeumReason
+
 
 # Number of counters displayed on the home page's best seumeurs graph
 bestSeumeursNumber = 15
@@ -34,52 +40,52 @@ def index(request):
     # First select our counter
     try:
         myCounter = Counter.objects.get(user__id=request.user.id)
-        lastReset = Reset.objects.filter(
-            counter=myCounter).order_by('-timestamp')
-        if (lastReset.count() == 0):
+        myLastReset = Reset.objects.select_related('who').filter(counter=myCounter).order_by('-timestamp').first()
+
+        if myLastReset is None:
             # This person never had the seum
             myCounter.lastReset = Reset()
             myCounter.lastReset.delta = no_seum_delta
             myCounter.lastReset.noSeum = True
         else:
-            myCounter.lastReset = lastReset[0]
+            myCounter.lastReset = myLastReset
             myCounter.lastReset.noSeum = False
-            if (myCounter.lastReset.who is None or
-                    myCounter.lastReset.who.id == myCounter.id):
+            if myCounter.lastReset.who is None or myCounter.lastReset.who.id == myCounter.id:
                 myCounter.lastReset.selfSeum = True
             else:
                 myCounter.lastReset.selfSeum = False
-            myCounter.lastReset.delta = datetime.now(
-            ) - myCounter.lastReset.timestamp.replace(tzinfo=None)
-            likesMe = Like.objects.filter(
-                reset=myCounter.lastReset)
-            myCounter.likeCount = likesMe.count()
-            if myCounter.likeCount:
-                myCounter.likersString = functools.reduce(
-                    lambda a, b: a + ", " + b,
-                    [like.liker.trigramme for like in likesMe])
-        myCounter.lastReset.formatted_delta = format_timedelta(
-            myCounter.lastReset.delta, locale='fr', threshold=1)
+            likesMe = list(Like.objects.select_related('liker').filter(reset=myCounter.lastReset))
+            myCounter.likeCount = len(likesMe)
+            if myCounter.likeCount > 0:
+                myCounter.likersString = ", ".join([like.liker.trigramme for like in likesMe])
+
+        myCounter.lastReset.formatted_delta = arrow.Arrow.fromdatetime(myCounter.lastReset.timestamp).humanize(locale=get_language())
+
     except Counter.DoesNotExist:
         return HttpResponseRedirect(reverse('login'))
 
     # Building data for counters display
-    counters = Counter.objects.all()
+    counters = Counter.objects.prefetch_related(
+        'resets__likes',
+        Prefetch(
+            'resets',
+            queryset=Reset.objects.prefetch_related('who', Prefetch('likes', queryset=Like.objects.select_related('liker'))).order_by('-timestamp'),
+            to_attr='lastReset'
+        )
+    )
     for counter in counters:
         # Only the last reset is displayed
-        lastReset = Reset.objects.filter(
-            counter=counter).order_by('-timestamp')
-        if (lastReset.count() == 0):  # This person never had the seum
+        lastReset = list(counter.lastReset)
+        if len(lastReset) == 0:  # This person never had the seum
             counter.lastReset = Reset()
             counter.lastReset.delta = no_seum_delta
             counter.lastReset.noSeum = True
+            counter.lastReset.likes_count = -1
             counter.CSSclass = "warning"
-            counter.likeCount = -1
         else:  # This person already had the seum
             counter.lastReset = lastReset[0]
             # To display the last seum we have to know if it is self-inflicted
-            if (counter.lastReset.who is None or
-                    counter.lastReset.who.id == counter.id):
+            if counter.lastReset.who is None or counter.lastReset.who == counter:
                 counter.lastReset.selfSeum = True
             else:
                 counter.lastReset.selfSeum = False
@@ -88,20 +94,13 @@ def index(request):
             counter.lastReset.delta = datetime.now(
             ) - counter.lastReset.timestamp.replace(tzinfo=None)
             # Defining CSS attributes for the counter
-            if counter.id == myCounter.id:
-                counter.CSSclass = 'primary'
-            else:
-                counter.CSSclass = 'default'
+            counter.CSSclass = 'primary' if counter == myCounter else 'default'
             # Computing the total number of likes for this counter
-            likesMe = Like.objects.filter(
-                reset=counter.lastReset)
-            counter.likeCount = likesMe.count()
-            counter.alreadyLiked = (Like.objects.filter(
-                reset=counter.lastReset, liker=myCounter).exists())
-            if counter.likeCount > 0:
-                counter.likersString = functools.reduce(
-                    lambda a, b: a + ", " + b,
-                    [like.liker.trigramme for like in likesMe])
+            likesMe = list(counter.lastReset.likes.all())
+            counter.lastReset.likes_count = len(likesMe)
+            counter.alreadyLiked = myCounter in likesMe
+            if counter.lastReset.likes_count > 0:
+                counter.likersString = ", ".join([like.liker.trigramme for like in likesMe])
 
         counter.lastReset.formatted_delta = format_timedelta(
             counter.lastReset.delta, locale='fr', threshold=1)
@@ -112,18 +111,14 @@ def index(request):
         # We take into account the number of likes of a reset and recentness
         # The log on the score will give increased value to the first likes
         # The counters with no seum have a like count of -1 by convention
-        counters = sorted(counters, key=lambda t: - (
-            math.log(t.likeCount + 2) /
-            (1 + (t.lastReset.delta.total_seconds()) /
-             (24 * 3600))))
+        sorting_key = lambda t: - (math.log(t.lastReset.likes_count + 2) / (1 + (t.lastReset.delta.total_seconds()) / (24 * 3600)))
+        counters = sorted(counters, key=sorting_key)
     else:
-        counters = sorted(counters, key=lambda t: +
-                          t.lastReset.delta.total_seconds())
+        counters = sorted(counters, key=lambda t: + t.lastReset.delta.total_seconds())
 
     # Timeline graph
-    resets = Reset.objects.filter(
-        timestamp__gte=timezone.now() - timedelta(days=1))
-    if (resets.count() == 0):
+    resets = Reset.objects.select_related('who', 'counter').filter(timestamp__gte=timezone.now() - timedelta(days=1))
+    if resets.count() == 0:
         noTimeline = True
         line_chart = None
     else:
