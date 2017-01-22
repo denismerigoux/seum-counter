13a08911ee5b163c68210709aea4e27874967a69
Denis Merigoux <denis.merigoux@gmail.com>
2017-01-22 13:16:25 -0500

    Fixing bugs tied to internationalization

--- seum/urls.py
+++ seum/urls.py
@@ -19,14 +19,14 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 
-urlpatterns = [url(r'^i18n/', include('django.conf.urls.i18n'), name='set_language'), ]
+urlpatterns = [
+    url(r'^i18n/', include('django.conf.urls.i18n'), name='set_language'),]
 
-urlpatterns += i18n_patterns(
+urlpatterns += [
     url(r'^admin/', admin.site.urls),
-
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
     url(r'^robots\.txt$', RedirectView.as_view(url='/static/robots.txt')),
-)
+]
 
 if settings.DEBUG:
     import debug_toolbar
@@ -34,4 +34,6 @@ if settings.DEBUG:
         url(r'^__debug__/', include(debug_toolbar.urls)),
     )
 
-urlpatterns += i18n_patterns(url(r'^', include('counter.urls')), )
+urlpatterns += i18n_patterns(
+    url(r'^', include('counter.urls')),
+)
