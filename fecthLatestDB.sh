8acb360d3ac274bdf35c29673c83823de804d59b
Denis Merigoux <denis.merigoux@gmail.com>
2017-04-10 14:04:31 -0400

    Permissions and updated database fetch script

--- fecthLatestDB.sh
+++ fecthLatestDB.sh
@@ -1,5 +1,5 @@
 cd "$(dirname "$0")"
-ssh evezh 'cd seum-counter; source env/bin/activate; python manage.py dumpdata --exclude contenttypes > seum.json'
+ssh evezh 'cd seum-counter; source env/bin/activate; python manage.py dumpdata --exclude auth.permission --exclude sessions.session --exclude admin.logentry --exclude contenttypes > seum.json'
 scp evezh:/home/denis/seum-counter/seum.json seum.json
 rm -f db.sqlite3
 source env/bin/activate
